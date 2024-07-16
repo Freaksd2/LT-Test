3bfd35cdb8efadfc232734510e0ddd0034cd89f2
Freaksd2 <117035956+Freaksd2@users.noreply.github.com>
2024-07-17 00:39:42 +0300

    Add files via upload

--- Task2/py_task2.py
+++ Task2/py_task2.py
@@ -1,9 +1,5 @@
 import math
-
-# calculate and output point status
-# c_pos - circle coordinates
-# c_r   - circle ratius
-# p_pos -  point coordinates
+import sys
 
 
 def check_point(c_pos, c_r, p_pos):
@@ -17,14 +13,14 @@ def check_point(c_pos, c_r, p_pos):
         print('2')
 
 
-with open(str('file1.txt'), 'r') as circle_file:
+with open(str(sys.argv[1]), 'r') as circle_file:
     temp = circle_file.readline().split(' ')
     circle_pos = [float(temp[0]), float(temp[1])]
 
     circle_rad = float(circle_file.readline())
 
 
-with open(str('file2.txt'), 'r') as points_file:
+with open(str(sys.argv[2]), 'r') as points_file:
     for line in points_file:
         temp = line.split(' ')
         check_point(circle_pos, circle_rad, [float(temp[0]), float(temp[1])])
